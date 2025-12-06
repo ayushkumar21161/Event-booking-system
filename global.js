2736d4f0c9d6b2096d4d3e80f8f8736e487916b1
ayushkumar21161 <bpil.ayush@gmail.com>
2025-12-06 21:12:40 +0530

    Fix typo in Services link filename

--- global.js
+++ global.js
@@ -147,7 +147,7 @@
     el.innerHTML = "";
     const links = [
       {text:"Home", href:"index.html"},
-      {text:"Services", href:"Our_services.html"},
+      {text:"Services", href:"Our_Services.html"},
       {text:"About", href:"about.html"},
       {text:"Contact", href:"contact.html"}
     ];
